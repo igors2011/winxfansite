d12b3703a77dc242b82bacf6a9a278b45a5b897d
igors <igors-2011@mail.ru>
2023-10-18 23:58:15 +0400

    init

---
+++ src/main/java/com/winxfansite/fansite/FansiteApplication.java
@@ -0,0 +1,13 @@
+package com.winxfansite.fansite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FansiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FansiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winxfansite/fansite/FansiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winxfansite.fansite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FansiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
