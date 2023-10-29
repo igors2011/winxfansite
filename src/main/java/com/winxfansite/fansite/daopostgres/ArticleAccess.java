a4fd3532280e7b3f087e8e6b2d4bc4db482932cc
igors <igors-2011@mail.ru>
2023-10-29 20:46:25 +0400

    dbconnection edit

--- src/main/java/com/winxfansite/fansite/daopostgres/ArticleAccess.java
+++ src/main/java/com/winxfansite/fansite/daopostgres/ArticleAccess.java
@@ -3,33 +3,22 @@ package com.winxfansite.fansite.daopostgres;
 import com.winxfansite.fansite.models.Article;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 @Component
 public class ArticleAccess {
-    private static final String URL = "jdbc:postgresql://localhost:5432/winxfansite";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
-    private static final Connection connection;
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            connection = DriverManager.getConnection(URL,USER,PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
     public List<Article> getArticles(String articleType) {
         List<Article> result = new ArrayList<>();
+        Connection connection = null;
         try {
+            connection = DBConnection.getConnection();
             Statement statement = connection.createStatement();
             String SQLQuery = "SELECT * FROM articles WHERE articletype = '" + articleType + "'";
             ResultSet resultSet = statement.executeQuery(SQLQuery);
@@ -37,28 +26,33 @@ public class ArticleAccess {
                 Article newArticle = new Article(resultSet.getInt("id"), resultSet.getString("header"), resultSet.getString("shortdescr"), resultSet.getString("longdescr"), resultSet.getString("articletype"), resultSet.getString("author"));
                 result.add(newArticle);
             }
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             throw new RuntimeException(e);
         }
         return result;
     }
+
     public Article getArticleById(int id) {
         Article result = null;
+        Connection connection = null;
         try {
+            connection = DBConnection.getConnection();
             Statement statement = connection.createStatement();
             String SQLQuery = "SELECT * FROM articles WHERE id = '" + id + "'";
             ResultSet resultSet = statement.executeQuery(SQLQuery);
             while (resultSet.next()) {
                 result = new Article(resultSet.getInt("id"), resultSet.getString("header"), resultSet.getString("shortdescr"), resultSet.getString("longdescr"), resultSet.getString("articletype"), resultSet.getString("author"));
             }
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             throw new RuntimeException(e);
         }
         return result;
     }
     public List<Article> findArticles(String query) {
         List<Article> result = new ArrayList<>();
+        Connection connection = null;
         try {
+            connection = DBConnection.getConnection();
             Statement statement = connection.createStatement();
             String SQLQuery = "SELECT * FROM articles WHERE header LIKE '%" + query + "%'";
             ResultSet resultSet = statement.executeQuery(SQLQuery);
@@ -66,7 +60,7 @@ public class ArticleAccess {
                 Article newArticle = new Article(resultSet.getInt("id"), resultSet.getString("header"), resultSet.getString("shortdescr"), resultSet.getString("longdescr"), resultSet.getString("articletype"), resultSet.getString("author"));
                 result.add(newArticle);
             }
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             throw new RuntimeException(e);
         }
         return result;
