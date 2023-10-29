3584e8b3365cf4f14725639d4196a13bef5f7c8d
igors <igors-2011@mail.ru>
2023-10-29 19:59:35 +0400

    folders fix

--- src/main/java/com/winxfansite/fansite/controllers/ArticlesController.java
+++ src/main/java/com/winxfansite/fansite/controllers/ArticlesController.java
@@ -1,7 +1,8 @@
 package com.winxfansite.fansite.controllers;
 
-import com.winxfansite.fansite.dataaccesspostgres.ArticleAccess;
+import com.winxfansite.fansite.daopostgres.ArticleAccess;
 import com.winxfansite.fansite.models.Article;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.sql.Connection;
 import java.util.List;
 
 @Controller
 @RequestMapping("/articles")
 public class ArticlesController {
     private final ArticleAccess articleAccess;
+    @Autowired
     public ArticlesController(ArticleAccess fairiesAccess) {
         this.articleAccess = fairiesAccess;
     }
