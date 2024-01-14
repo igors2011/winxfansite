2a75d5a7a6fbe7e4dfe47008ef02dcaeed6a3166
igors <igors-2011@mail.ru>
2024-01-14 17:38:27 +0400

    temporary turning off spring security

--- user-mvc/src/main/java/com/winxfansite/usermvc/SecurityConfig.java
+++ user-mvc/src/main/java/com/winxfansite/usermvc/SecurityConfig.java
@@ -1,41 +1,41 @@
-package com.winxfansite.usermvc;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/articles")
-                .permitAll()
-                .antMatchers("/search")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/account/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
-
-    }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("user")
-                .roles("user")
-                .and()
-                .withUser("editor")
-                .password("editor")
-                .roles("editor");
-    }
-}
+//package com.winxfansite.usermvc;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/articles")
+//                .permitAll()
+//                .antMatchers("/search")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/account/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
+//
+//    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user")
+//                .password("user")
+//                .roles("user")
+//                .and()
+//                .withUser("editor")
+//                .password("editor")
+//                .roles("editor");
+//    }
+//}
--- user-mvc/src/main/java/com/winxfansite/usermvc/implementations/CustomUserDetails.java
+++ user-mvc/src/main/java/com/winxfansite/usermvc/implementations/CustomUserDetails.java
@@ -1,48 +1,48 @@
-package com.winxfansite.usermvc.implementations;
-
-import models.User;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-
-public class CustomUserDetails implements UserDetails {
-    private User user;
-    private CustomUserDetails(User user) {
-        this.user = user;
-    }
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return user.getLogin();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-}
+//package com.winxfansite.usermvc.implementations;
+//
+//import models.User;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Collection;
+//
+//public class CustomUserDetails implements UserDetails {
+//    private User user;
+//    private CustomUserDetails(User user) {
+//        this.user = user;
+//    }
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return user.getPassword();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return user.getLogin();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return false;
+//    }
+//}
