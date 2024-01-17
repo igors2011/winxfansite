414a0bac222cbd9d9481941f72a8c28602d29b13
igors <igors-2011@mail.ru>
2024-01-17 15:13:50 +0400

    adding writing log logic

--- models/src/main/java/models/Log.java
+++ models/src/main/java/models/Log.java
@@ -3,7 +3,46 @@ package models;
 import java.sql.Date;
 
 public class Log {
+    private int id;
+
+    public Log(Date date, String type, String message) {
+        this.date = date;
+        this.type = type;
+        this.message = message;
+    }
+
     private Date date;
     private String type;
     private String message;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- user-mvc/src/main/java/com/winxfansite/usermvc/daopostgres/LogAccess.java
+++ user-mvc/src/main/java/com/winxfansite/usermvc/daopostgres/LogAccess.java
@@ -1,4 +1,39 @@
 package com.winxfansite.usermvc.daopostgres;
 
+import models.Log;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Component
 public class LogAccess {
+    public static PreparedStatement prepareLog(Connection connection, Log log) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO logs (date, type, message) VALUES (?, ?, ?)");
+            statement.setDate(1, log.getDate());
+            statement.setString(2, log.getType());
+            statement.setString(3, log.getMessage());
+            return statement;
+        }
+        catch (Exception e)
+        {
+            throw  new RuntimeException(e);
+        }
+    }
+    public void insertLog(Log log) {
+        try {
+            Connection connection = DBConnection.getConnection();
+            PreparedStatement preparedLog = prepareLog(connection, log);
+            preparedLog.executeUpdate();
+            preparedLog.close();
+            connection.close();
+        }
+        catch (SQLException | IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
 }
