89769dec3d1761907a363f7a16e46b61610a0af9
igors <igors-2011@mail.ru>
2024-01-17 13:53:32 +0400

    creating admin app

---
+++ admin/src/main/java/com/winxfansite/admin/FansiteApplication.java
@@ -0,0 +1,11 @@
+package com.winxfansite.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FansiteApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FansiteApplication.class, args);
+	}
+}
---
+++ admin/src/main/java/com/winxfansite/admin/dao/DBConnection.java
@@ -0,0 +1,25 @@
+package com.winxfansite.admin.dao;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection
+{
+    public static Connection getConnection() throws SQLException, IOException {
+        Properties props = new Properties();
+        try(InputStream in = Files.newInputStream(Paths.get("admin/src/main/resources/settings/dbconnection.properties"))){
+            props.load(in);
+        }
+        String url = props.getProperty("url");
+        String username = props.getProperty("username");
+        String password = props.getProperty("password");
+        return DriverManager.getConnection(url, username, password);
+    }
+}
