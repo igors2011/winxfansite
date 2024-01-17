0ede74bd1f1bb57564926eeac6db8f3ddc12af0e
evasina2312@gmail.com <evasina2312@gmail.com>
2024-01-17 21:59:48 +0400

    fixed_head

--- src/main/resources/static/js/header_script.js
+++ src/main/resources/static/js/header_script.js
@@ -3,15 +3,20 @@ document.getElementById("open_menu").addEventListener("click", openCloseMenu, fa
 if (document.documentElement.clientWidth < 1000)
 {
     var menu = document.getElementById("menu_list");
+    var search = document.getElementById("search_form");
     menu.className = "nav_hide";
+    search.className = "search_hide";
 }
 
 function openCloseMenu() {
     var menu = document.getElementById("menu_list");
+    var search = document.getElementById("search");
     if (menu.className != "nav") {
         menu.className = "nav";
+        search.className = "search";
     }
     else {
         menu.className = "nav_hide";
+        search.className = "search_hide";
     }
 }
